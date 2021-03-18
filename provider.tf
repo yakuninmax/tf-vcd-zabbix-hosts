aa2e22ab203ca68d5dff49433b95457597112be7
Max Yakunin <yakunin.max@gmail.com>
2021-03-18 13:06:38 +0300

    Initial commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "zabbix" {
-  user       = var.admin_user
+  user       = var.admin_name
   password   = var.admin_password
-  server_url = "http://${var.external-ip}:${var.external_http_port}/zabbix/api_jsonrpc.php"
+  server_url = "http://${var.external_ip}:${var.external_http_port}/zabbix/api_jsonrpc.php"
 }
